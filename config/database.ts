ef0313e0a8c9a6ee202694eb9acebe482ff8a9ff
Shweta Patel <shwetap3032003@gmail.com>
2026-05-17 22:29:52 +0530

    fix render deploy config

--- config/database.ts
+++ config/database.ts
@@ -1,37 +1,50 @@
-export default ({ env }) => {
-  const client = env("DATABASE_CLIENT", "postgres");
+// export default ({ env }) => {
+//   const client = env("DATABASE_CLIENT", "postgres");
 
-  const connections = {
-    sqlite: {
-      connection: {
-        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
-      },
-      useNullAsDefault: true,
-    },
+//   const connections = {
+//     sqlite: {
+//       connection: {
+//         filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+//       },
+//       useNullAsDefault: true,
+//     },
 
-    postgres: {
-      connection: {
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
-        user: env("DATABASE_USERNAME", "postgres"),
-        password: env("DATABASE_PASSWORD", ""),
-        ssl: env.bool("DATABASE_SSL", false)
-          ? { rejectUnauthorized: false }
-          : false,
-      },
-      pool: {
-        min: env.int("DATABASE_POOL_MIN", 2),
-        max: env.int("DATABASE_POOL_MAX", 10),
-      },
-    },
-  };
+//     postgres: {
+//       connection: {
+//         host: env("DATABASE_HOST", "localhost"),
+//         port: env.int("DATABASE_PORT", 5432),
+//         database: env("DATABASE_NAME", "strapi"),
+//         user: env("DATABASE_USERNAME", "postgres"),
+//         password: env("DATABASE_PASSWORD", ""),
+//         ssl: env.bool("DATABASE_SSL", false)
+//           ? { rejectUnauthorized: false }
+//           : false,
+//       },
+//       pool: {
+//         min: env.int("DATABASE_POOL_MIN", 2),
+//         max: env.int("DATABASE_POOL_MAX", 10),
+//       },
+//     },
+//   };
 
-  return {
+//   return {
+//     connection: {
+//       client,
+//       ...connections[client],
+//       acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+//     },
+//   };
+// };
+
+export default ({ env }) => ({
+  connection: {
+    client: env("DATABASE_CLIENT", "postgres"),
     connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+      connectionString: env("DATABASE_URL"),
+      ssl: env.bool("DATABASE_SSL", true)
+        ? { rejectUnauthorized: false }
+        : false,
     },
-  };
-};
+    acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+  },
+});
--- config/server.ts
+++ config/server.ts
@@ -1,11 +1,19 @@
-import type { Core } from '@strapi/strapi';
+// import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 10000),
+// const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 10000),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+// });
+
+// export default config;
+
+export default ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 10000),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
 });
-
-export default config;
