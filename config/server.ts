53944bf766efc6c07743388149af3fe11b40b7fc
Shweta Patel <shwetap3032003@gmail.com>
2026-05-20 14:02:33 +0530

    fix render port

--- config/server.ts
+++ config/server.ts
@@ -10,18 +10,11 @@
 
 // export default config;
 
-// export default ({ env }) => ({
-//   host: env("HOST", "0.0.0.0"),
-//   port: env.int("PORT", 10000),
-//   app: {
-//     keys: env.array("APP_KEYS"),
-//   },
-// });
-
 export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
+  port: env.int("PORT", 10000),
   app: {
     keys: env.array("APP_KEYS"),
   },
 });
+
